8b885a55132e106b91b21ead91bdf344a9764339
qunda <qunda.lim@outlook.sg>
2020-03-04 15:22:32 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,22 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.")
+		console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId) {
+	let flipped = cards[cardId];
+	console.log(`User flipped ${flipped}`);
+	cardsInPlay.push(flipped);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
